2e60a4eb076e7f3412dbf6976bf31f4cd1feec44
Eduardo Monforte <eduardomonforte@gmail.com>
2018-09-19 14:00:36 -0500

    Added time-based music and sound effects.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,14 +4,26 @@ console.log("I'm linked!")
 
 var bgMusic = document.createElement("audio");
     bgMusic.setAttribute("src", "assets/audio/music/overworld.mp3");
-/*    bgMusic.autoplay = true;
-    bgMusic.load();
-    bgMusic.addEventListener("load", function() {
-        bgMusic.play();
-    }, true); */
+var hurryMusic = document.createElement("audio");
+    hurryMusic.setAttribute("src", "assets/audio/music/hurry.mp3")
+var loseLife = document.createElement("audio");
+    loseLife.setAttribute("src", "assets/audio/sounds/loselife.wav")
 
 $("#play-button").on("click", function() {
+
     bgMusic.play();
+    function thirtySeconds() {
+        bgMusic.pause();
+        hurryMusic.play();
+    }
+    function sixtySeconds() {
+        hurryMusic.pause();
+        loseLife.play();
+    }
+
+    setTimeout (thirtySeconds, 30000);
+    setTimeout (sixtySeconds, 60000)
+
 })
 
 }
