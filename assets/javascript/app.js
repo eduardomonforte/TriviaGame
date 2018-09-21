4db4db622c4673841aae54cc76f3906d9e83c0e8
Eduardo Monforte <eduardomonforte@gmail.com>
2018-09-20 23:05:39 -0500

    Added new variables for later use. Optimized the way existing functions work. Cleaned up the code and added more comments.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,81 +1,105 @@
 $(document).ready(); {
 
-console.log("BOWSER WAS HERE!");
+    console.log("BOWSER WAS HERE!");
 
-var round = 1;
-var running = false;
+    // Initial State Variables
 
-$("#trivia-container").hide();
+    var round = 1;
+    var question = 1;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+    var running = false;
 
-// Audio Variables:
+    // Audio Variables
 
-var bgMusic = document.createElement("audio");
+    var bgMusic = document.createElement("audio");
     bgMusic.setAttribute("src", "assets/audio/music/overworld.mp3");
-var hurryMusic = document.createElement("audio");
+    bgMusic.load();
+    var hurryMusic = document.createElement("audio");
     hurryMusic.setAttribute("src", "assets/audio/music/overworld-hurry.mp3");
-var loseLife = document.createElement("audio");
+    hurryMusic.load();
+    var loseLife = document.createElement("audio");
     loseLife.setAttribute("src", "assets/audio/sounds/lose-life.wav");
+    loseLife.load();
 
-// This function runs when you press the Enter key:
+    // The interface requires some elements to be hidden by default:
+
+    $("#trivia-container").hide();
+    $("#flagpole-down").hide();
+
+    // This function runs when you press a key:
 
     $(document).keyup(function(keyPressed) {
 
-    if (keyPressed.keyCode == 13 && running == false) {
+        // This function runs only if you press the Enter key:
 
-    running = true;
+        if (keyPressed.keyCode == 13 && running == false) {
 
-    var time = 60;
-    var intervalId;
+            // The boolean changes when the game is running.
+            // Ths prevents the Enter key from having any effect during the game.
 
-    function run() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
-        $("#trivia-container").show();
-        $("#question-round").html(round++);
-        $("#press-enter").hide();
-        $("#logo").hide();
-        bgMusic.load();
-        hurryMusic.load();
-        loseLife.load();
-        bgMusic.play();
-        function fortySeconds() {
-            bgMusic.pause();
-            hurryMusic.play();
+            running = true;
+
+            var time = 60;
+            var intervalId;
+
+            run();
+
+            function run() {
+                
+                clearInterval(intervalId);
+                intervalId = setInterval(decrement, 1000);
+                $("#trivia-container").show();
+                $("#question-round").html(round++);
+                $("#press-enter").hide();
+                $("#logo").hide();
+                bgMusic.load();
+                hurryMusic.load();
+                loseLife.load();
+                bgMusic.play();
+
+            }
+
+            function decrement() {
+
+                time--;
+                $("#time-left").html(time);
+
+                if (time < 10) {
+
+                    $("#time-left").html("0" + time);
+
+                }
+
+                if (time === 20) {
+
+                    bgMusic.pause();
+                    hurryMusic.play();
+
+                }
+
+                if (time === 0) {
+
+                    clearInterval(intervalId);
+                    hurryMusic.pause();
+                    loseLife.play();
+
+                    loseLife.onended = function() {
+
+                        $("#press-enter").show();
+                        $("#logo").show();
+                        $("#trivia-container").hide();
+                        $("#time-left").html("60");
+                        running = false;
+
+                    }
+
+                }
+
+            }
+
         }
-        function sixtySeconds() {
-            hurryMusic.pause();
-        }
-
-        setTimeout (fortySeconds, 40000);
-        setTimeout (sixtySeconds, 60000);
-    }
-
-    function decrement() {
-        time--;
-        $("#time-left").html(time);
-        if (time < 10) {
-            $("#time-left").html("0" + time);
-        }
-        if (time === 0) {
-            stop();
-            loseLife.play();
-            loseLife.onended = function() {
-            $("#press-enter").show();
-            $("#logo").show();
-            $("#trivia-container").hide();
-            $("#time-left").html("60");
-            running = false;
-        }
-        }
-    }
-
-    function stop() {
-        clearInterval(intervalId);
-    }
-
-    run();
-
-    }
 
     });
 
